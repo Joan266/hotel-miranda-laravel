b61f42cfb5f84deb6c326abfc77b84aa97f04c2c
Joan266 <joanalemany26@gmail.com>
2024-09-20 08:35:44 +0200

    rooms view dinamic

--- resources/views/pages/rooms/rooms.blade.php
+++ resources/views/pages/rooms/rooms.blade.php
@@ -1,60 +1,22 @@
 <section class="rooms">
     <div class="rooms__grid container">
+        @foreach ($rooms as $room)
         <div class="rooms__grid__item">
             <div class="rooms__grid__item__features">
                 <img class="rooms__grid__item__features__icons" src="{{ asset('icon/hotel-room-features 1.svg') }}" alt="hotel room features">
-                <img class="rooms__grid__item__features__img" src="{{ asset('img/room 1.webp') }}" alt="room 1">
+                <img class="rooms__grid__item__features__img" src="{{ asset('img/' . $room->image) }}" alt="{{ $room->name }}">
             </div>
             <div class="rooms__grid__item__description">
-                <h4 class="title title--black title--small">Minimal Duplex Room</h4>
-                <p class="text">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor. Lorem ipsum dolor sit amet.</p>
+                <h4 class="title title--black title--small">{{ $room->name }}</h4>
+                <p class="text">{{ $room->description }}</p>
                 <div>
-                    <span class="rooms__grid__item__description__price">$345<span>/Night</span></span>
-                    <a href="roomdetails.html"><span class="rooms__grid__item__description__booking">Booking Now</span></a>
+                    <span class="rooms__grid__item__description__price">${{ $room->rate }}<span>/Night</span></span>
+                    <a href="{{ route('rooms.show', $room->id) }}">
+                        <span class="rooms__grid__item__description__booking">Booking Now</span>
+                    </a>
                 </div>
             </div>
         </div>
-        <div class="rooms__grid__item">
-            <div class="rooms__grid__item__features">
-                <img class="rooms__grid__item__features__icons" src="{{ asset('icon/hotel-room-features 1.svg') }}" alt="hotel room features">
-                <img class="rooms__grid__item__features__img" src="{{ asset('img/room 1.webp') }}" alt="room 1">
-            </div>
-            <div class="rooms__grid__item__description">
-                <h4 class="title title--black title--small">Minimal Duplex Room</h4>
-                <p class="text">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor. Lorem ipsum dolor sit amet.</p>
-                <div>
-                    <span class="rooms__grid__item__description__price">$345<span>/Night</span></span>
-                    <a href="roomdetails.html"><span class="rooms__grid__item__description__booking">Booking Now</span></a>
-                </div>
-            </div>
-        </div>
-        <div class="rooms__grid__item">
-            <div class="rooms__grid__item__features">
-                <img class="rooms__grid__item__features__icons" src="{{ asset('icon/hotel-room-features 1.svg') }}" alt="hotel room features">
-                <img class="rooms__grid__item__features__img" src="{{ asset('img/room 1.webp') }}" alt="room 1">
-            </div>
-            <div class="rooms__grid__item__description">
-                <h4 class="title title--black title--small">Minimal Duplex Room</h4>
-                <p class="text">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor. Lorem ipsum dolor sit amet.</p>
-                <div>
-                    <span class="rooms__grid__item__description__price">$345<span>/Night</span></span>
-                    <a href="roomdetails.html"><span class="rooms__grid__item__description__booking">Booking Now</span></a>
-                </div>
-            </div>
-        </div>
-        <div class="rooms__grid__item">
-            <div class="rooms__grid__item__features">
-                <img class="rooms__grid__item__features__icons" src="{{ asset('icon/hotel-room-features 1.svg') }}" alt="hotel room features">
-                <img class="rooms__grid__item__features__img" src="{{ asset('img/room 1.webp') }}" alt="room 1">
-            </div>
-            <div class="rooms__grid__item__description">
-                <h4 class="title title--black title--small">Minimal Duplex Room</h4>
-                <p class="text">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor. Lorem ipsum dolor sit amet.</p>
-                <div>
-                    <span class="rooms__grid__item__description__price">$345<span>/Night</span></span>
-                    <a href="roomdetails.html"><span class="rooms__grid__item__description__booking">Booking Now</span></a>
-                </div>
-            </div>
-        </div>
+        @endforeach
     </div>
 </section>
