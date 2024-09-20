85433080ccdbef9e37ecbe7347a41c8c530ab6b1
Joan266 <joanalemany26@gmail.com>
2024-09-20 09:04:53 +0200

    added tostify

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,28 +1,63 @@
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <title>Hotel Miranda - @yield('title')</title>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        @vite([
-            'resources/sass/common.scss',
-            'resources/sass/header.scss',
-            'resources/sass/footer.scss',
-            'resources/js/app.js'
-        ])
-        @stack('styles')
-      </head>
-    <body>
-         @include('partials.header')  
+<head>
+    <title>Hotel Miranda - @yield('title')</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    @vite([
+        'resources/sass/common.scss',
+        'resources/sass/header.scss',
+        'resources/sass/footer.scss',
+        'resources/js/app.js'
+    ])
 
-        @yield('content')
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/toastify-js/src/toastify.min.css">
+    @stack('styles')
+</head>
+<body>
+    @include('partials.header')  
 
-        @include('partials.footer') 
-        
-        @vite([
-            'resources/js/menu.js'
-         ])
-        @stack('scripts')
-        
-    </body>
+    @yield('content')
+
+    @include('partials.footer') 
+
+    <script src="https://cdn.jsdelivr.net/npm/toastify-js"></script>
+    <script>
+        document.addEventListener("DOMContentLoaded", function() {
+            @if (session('error'))
+                Toastify({
+                    className: "toastError",
+                    text: "{{ session('error') }}",
+                    duration: 3000, // Duration of the notification
+                    gravity: "top", // Position of the notification (top or bottom)
+                    offset: {
+                        y: 150
+                    },
+                    style: {
+                        background: "linear-gradient(to right, #FF5F6D, #FFC371)"
+                    }
+                }).showToast();
+            @endif
+            @if (session('success'))
+                Toastify({
+                    className: "toastSuccess",
+                    text: "{{ session('success') }}",
+                    duration: 3000, // Duration of the notification
+                    gravity: "top", // Position of the notification (top or bottom)
+                    offset: {
+                        y: 150
+                    },
+                    style: {
+                        background: "linear-gradient(to right, #00b09b, #96c93d)"
+                    }
+                }).showToast();
+            @endif
+        });
+    </script>
+
+    @vite([
+        'resources/js/menu.js'
+    ])
+    @stack('scripts')
+</body>
 </html>
