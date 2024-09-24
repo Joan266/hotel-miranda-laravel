15884ff3a23cda42f6d029159133ac140c3aae1b
Joan266 <joanalemany26@gmail.com>
2024-09-24 18:42:35 +0200

    refactored rooms style

--- resources/views/pages/rooms/rooms.blade.php
+++ resources/views/pages/rooms/rooms.blade.php
@@ -1,25 +1,22 @@
 <section class="rooms">
-    <h3 class="subtitle">ROOMS</h3>
-    <h4 class="title">Hand Picked Rooms</h4>
-    <div class="swiper swiper--rooms">
-        <div class="swiper-wrapper">
-            @foreach ($roomTypes as $roomType)
-            <div class="swiper-slide">
-                <img class="swiper-slide__room-features" src="{{ asset('icon/hotel-room-features 1.svg') }}" alt="hotel room features">
-                <img class="swiper-slide__img" src="{{ asset($roomType->image) }}" alt="{{ $roomType->name }}">
-                <div class="swiper-slide__room-description">
-                    <div>
-                        <h5 class="title title--small">{{ $roomType->name }}</h5>
-                        <p class="text">
-                            Relax in our cozy, well-appointed room featuring modern amenities, a plush bed, free Wi-Fi, and a flat-screen TV. Enjoy a serene space perfect for unwinding or getting work done, complete with a private bathroom and complimentary toiletries.
-                        </p>
-                    </div>
-                    <span class="swiper-slide__room-description__price">${{ $roomType->price_per_night }}<span>/Night</span></span>
+    <div class="rooms__grid container">
+        @foreach ($roomTypes as $roomType)
+        <div class="rooms__grid__item">
+            <img class="rooms__grid__item__img" src="{{ asset($roomType->image) }}" alt="{{ $roomType->name }}">
+            <div class="rooms__grid__item__description">
+                <img class="rooms__grid__item__description__icons" src="{{ asset('icon/hotel-room-features 1.svg') }}" alt="Hotel room features">
+                <h4 class="title title--black title--medium">{{ $roomType->name }}</h4>
+                <p class="text">
+                    Relax in our cozy, well-appointed room featuring modern amenities, a plush bed, free Wi-Fi, and a flat-screen TV. Enjoy a serene space perfect for unwinding or getting work done, complete with a private bathroom and complimentary toiletries. Your comfort is our priority.
+                </p>
+                <div>
+                    <span class="rooms__grid__item__description__price">${{ $roomType->price_per_night }}<span>/Night</span></span>
+                    <a href="{{ route('roomdetails', ['id' => $roomType->id]) }}">
+                        <span class="rooms__grid__item__description__booking">RESERVE NOW</span>
+                    </a>
                 </div>
             </div>
-            @endforeach
         </div>
-        <div class="swiper-button-prev"></div>
-        <div class="swiper-button-next"></div>
+        @endforeach
     </div>
 </section>
