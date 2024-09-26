ff4011488d45aebebf86d1cda38f4cf067af94b7
Joan266 <joanalemany26@gmail.com>
2024-09-26 15:11:05 +0200

    updated intros titles

--- resources/views/pages/contact/page-intro.blade.php
+++ resources/views/pages/contact/page-intro.blade.php
@@ -1,7 +1,7 @@
 <div class="page-intro">
   <div class="container">
-      <h1 class="subtitle subtitle--white">THE ULTIMATE LUXURY</h1>
-      <h2 class="title title--white">New Details</h2>
-      <div class="page-intro__path-info">Home | <span>Blog</span></div>
+      <h1 class="subtitle subtitle--white">WE VALUE YOUR FEEDBACK</h1>
+      <h2 class="title title--white">Contact Us</h2>
+      <div class="page-intro__path-info">Home | <span>Contact</span></div>
   </div>
 </div>
--- resources/views/pages/roomdetails/page-intro.blade.php
+++ resources/views/pages/roomdetails/page-intro.blade.php
@@ -1,7 +1,7 @@
 <div class="page-intro">
     <div class="container">
-        <h1 class="subtitle subtitle--white">THE ULTIMATE LUXURY</h1>
-        <h2 class="title title--white">Ultimate Room</h2>
+        <h1 class="subtitle subtitle--white">ELEGANCE AND COMFORT</h1>
+        <h2 class="title title--white">Room Features and Highlights</h2>
         <div class="page-intro__path-info">Home | <span>Room Details</span></div>
     </div>
 </div>
